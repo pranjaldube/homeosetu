74d2008bd654e3cb20f0d3ad8ca3004f3049321b
Pranjal Pratap Dubey <keshavdubey29@gmail.com>
2024-12-16 22:14:31 +0530

    Add: Posthog analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import { ToastProvider } from "@/components/providers/toaster-provider"
 import { ConfettiProvider } from "@/components/providers/confetti-provider"
 import { Announcements } from "@/components/announcements" // Adjust this path if necessary
+import { CSPostHogProvider } from "@/components/providers/posthog-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,19 +20,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Announcements />
-        <div className="flex flex-col min-h-screen">
-          <div className="flex-grow pt-10">
-            {" "}
-            {/* Adjusted to account for the fixed Announcements bar */}
-            <ConfettiProvider />
-            <ToastProvider />
-            {children}
+    <CSPostHogProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Announcements />
+          <div className="flex flex-col min-h-screen">
+            <div className="flex-grow pt-10">
+              {" "}
+              {/* Adjusted to account for the fixed Announcements bar */}
+              <ConfettiProvider />
+              <ToastProvider />
+              {children}
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </CSPostHogProvider>
   )
 }
