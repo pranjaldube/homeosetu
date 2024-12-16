ed125c323a1083f6ad2fc5b01c25986d6b9fadbd
Pranjal Pratap Dubey <keshavdubey29@gmail.com>
2024-12-16 18:18:17 +0530

    Fix: Clerk middleware

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,11 @@
-const AuthLayout = ({
-  children
-}: {
-  children: React.ReactNode
-}) => {
-  return ( 
-    <div className="h-full flex items-center justify-center">
-      {children}
-    </div>
-   );
+import { ClerkProvider } from "@clerk/nextjs"
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <div className="h-full flex items-center justify-center">{children}</div>
+    </ClerkProvider>
+  )
 }
- 
-export default AuthLayout;
+
+export default AuthLayout
