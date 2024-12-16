9100dcfffd20dbfac3a39b1f24bde40475802d71
Pranjal Pratap Dubey <keshavdubey29@gmail.com>
2024-12-16 17:56:15 +0530

    Fix: Clerk Redirections

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,24 +1,21 @@
-import { Navbar } from "./_components/navbar";
-import { Sidebar } from "./_components/sidebar";
+import { Navbar } from "./_components/navbar"
+import { Sidebar } from "./_components/sidebar"
+import { ClerkProvider } from "@clerk/nextjs"
 
-const DashboardLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
-    <div className="h-full">
-      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
-        <Navbar />
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <div className="h-full">
+        <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+          <Navbar />
+        </div>
+        <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+          <Sidebar />
+        </div>
+        <main className="md:pl-56 pt-[80px] h-full">{children}</main>
       </div>
-      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-        <Sidebar />
-      </div>
-      <main className="md:pl-56 pt-[80px] h-full">
-        {children}
-      </main>
-    </div>
-   );
+    </ClerkProvider>
+  )
 }
- 
-export default DashboardLayout;
+
+export default DashboardLayout
