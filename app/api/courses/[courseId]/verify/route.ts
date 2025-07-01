ab7e2ab1d9fdebc4b42d9c3ba84005aeeae76de4
Jay Mawari <jaymawari31@gmail.com>
2025-07-01 15:13:02 +0530

    invoice code review

--- app/api/courses/[courseId]/verify/route.ts
+++ app/api/courses/[courseId]/verify/route.ts
@@ -1,8 +1,13 @@
 import { currentUser } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
+import axios from "axios"
 import crypto from "crypto"
 
 import { db } from "@/lib/db"
+
+function formatDate(date: Date): string {
+  return date.toLocaleDateString("en-GB").split("/").join("-"); // dd-mm-yyyy
+}
 
 export async function POST(
   req: Request,
@@ -42,13 +47,55 @@ export async function POST(
       return new NextResponse("Invalid signature", { status: 400 })
     }
 
+    const now = new Date()
+    const date = formatDate(now);
+    const course_price = course.price
+    const course_price_with_tax = course_price + Math.round((course_price/100) * 18)
+    
+    const invoicePayload = {
+      document_type: "invoice",
+      document_date: date,
+      due_date: date,
+      party:{
+        id: user.id,
+        type: "customer",
+        name: `${user?.firstName} ${user?.lastName}`,
+        email: user.emailAddresses?.[0]?.emailAddress
+      },
+      items:[
+        {
+          id: params.courseId,
+          name: course.title,
+          quantity: 1,
+          unit_price: course_price,
+          tax_rate: 18,
+          price_with_tax: course_price_with_tax,
+          net_amount: course_price,
+          total_amount: course_price_with_tax,
+          item_type: "Product"
+        }
+      ]
+    }
+
+    const invoiceResponse = await axios.post("https://app.getswipe.in/api/partner/v2/doc",
+      invoicePayload,
+      {
+        headers:{
+          Authorization: `Bearer ${process.env.SWIPE_INVOICE_KEY}`,
+          "Content-Type": "application/json"
+        }
+      },
+    )
+
+    const swipeHashId = invoiceResponse.data.data.hash_id
     // Create the purchase record
     await db.purchase.create({
       data: {
         courseId: params.courseId,
         userId: user.id,
+        swipeHashId,
       },
-    })
+    })  
 
     return new NextResponse("Payment verified", { status: 200 })
   } catch (error) {
