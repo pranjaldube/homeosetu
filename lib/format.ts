82511582fcf1a58ccfc3986566a1cab49df311bb
Jay Mawari <jaymawari31@gmail.com>
2025-07-09 16:16:06 +0530

    format price

--- lib/format.ts
+++ lib/format.ts
@@ -1,4 +1,4 @@
-export const formatPrice = (price: number, country: string) => {
+export const formatPrice = (price: number, country: string | undefined) => {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: (!country || country === "India") ? "INR" : "USD",
