4ea72c0aa43c0b3f00fbe608e3d76d3079a75311
Jay Mawari <jaymawari31@gmail.com>
2025-07-17 11:45:51 +0530

    changes in payment page accordingly on basis of location

--- lib/format.ts
+++ lib/format.ts
@@ -1,21 +1,13 @@
 export const formatPrice = (price: number | null) => {
+
+  if(price === null){
+    return ""
+  }
   // Read currency from cookie
   const currency = document.cookie
     .split("; ")
     .find((c) => c.startsWith("preferred_currency="))
     ?.split("=")[1] || "INR";
-
-  // let convertedPrice = price;
-
-  // // Static conversion rate (you can later fetch from API if needed)
-  // const conversionRates: Record<string, number> = {
-  //   INR: 1,
-  //   USD: 1 / 85, // Example: ₹83 = $1
-  // };
-
-  // if (currency === "USD") {
-  //   convertedPrice = price * conversionRates.USD;
-  // }
 
   return new Intl.NumberFormat("en-US", {
     style: "currency",
