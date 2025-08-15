60bbc9c89709cfb9529b48ee3de8705454b1c5b2
Jay Mawari <jaymawari31@gmail.com>
2025-08-15 16:25:12 +0530

    mail body and subject change

--- app/api/emailSchedule/route.ts
+++ app/api/emailSchedule/route.ts
@@ -33,34 +33,45 @@ export async function POST(req: Request) {
 
     await sendEmail({
             to: email,
-            subject: 'Clinical Resources PDF : Observations Compiled by Homeosetu',
-            text: `Dear Dr ${name},
-    
-    In homeopathic case taking, non-verbal clues are often as revealing as spoken words. These subtle signals help the practitioner perceive the patient's inner state, temperament, and even the peculiarities that guide remedy selection. 
-    
-    Attached PDF shall give you a structured overview.
-    
-    We also have some Video Study Courses Published on our website : www.homeosetu.com
-    You can Use PDF300 for an instant discount valid for next 48 hours on all our video courses.
-    
-    Regards
-    Team Homeosetu`,
+            subject: 'Clinical Resources PDF : Freedom from Prejudices-Compiled by Homeosetu',
+            text: `Dear Dr Alpesh ,
+
+A Homeopath always gets prejudiced in Case taking through reference of a single repertory / MM / Method.
+
+Attached PDF shall give you a structured overview for breaking your prejudices.
+
+We also have some Video Study Courses published on our website: www.homeosetu.com.
+
+You can use PDF300 for an instant discount valid for the next 48 hours on all our video courses.
+
+Regards,
+Team Homeosetu`,
             html: `
-        <p>Dear Dr ${name},</p>
-    
-        <p>In homeopathic case taking, <strong>non-verbal clues</strong> are often as revealing as spoken words. These subtle signals help the practitioner perceive the patient's inner state, temperament, and even the peculiarities that guide remedy selection.</p>
-    
-        <p>Attached PDF shall give you a structured overview.</p>
-    
-        <p>We also have some <strong>Video Study Courses</strong> published on our website: 
-          <a href="https://www.homeosetu.com" target="_blank">www.homeosetu.com</a>.
-        </p>
-    
-        <p>You can use <strong>PDF300</strong> for an instant discount valid for the next <strong>48 hours</strong> on all our video courses.</p>
-    
-        <p>Regards,<br/>
-        Team Homeosetu</p>
-      `,
+    <p>Dear Dr Alpesh,</p>
+
+    <p>
+      A Homeopath always gets prejudiced in Case taking through reference of a
+      single repertory / MM / Method.
+    </p>
+
+    <p>
+      Attached PDF shall give you a structured overview for breaking your
+      prejudices.
+    </p>
+
+    <p>
+      We also have some <strong>Video Study Courses</strong> published on our
+      website:
+      <a href="https://www.homeosetu.com" target="_blank">www.homeosetu.com</a>.
+    </p>
+
+    <p>
+      You can use <strong>PDF300</strong> for an instant discount valid for the
+      next <strong>48 hours</strong> on all our video courses.
+    </p>
+
+    <p>Regards,<br/>Team Homeosetu</p>
+  `,
             pdfFilePath: pdfPath,
           });
 
