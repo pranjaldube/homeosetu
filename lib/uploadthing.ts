f421b9096657c0dc0319be9651056a70b4f15d85
Jay Mawari <jaymawari31@gmail.com>
2025-08-18 14:50:38 +0530

    upload thing updated version with new uploadthing token

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,6 +1,5 @@
-import { generateComponents } from "@uploadthing/react";
- 
+import { UploadButton, UploadDropzone, Uploader } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
- 
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+
+// You can still re-export for consistency if you like
+export { UploadButton, UploadDropzone, Uploader };
