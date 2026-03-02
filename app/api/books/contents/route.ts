366871922b02b36ff575f5a54d054b2be3b377f0
Jay Mawari <jaymawari31@gmail.com>
2026-03-02 17:39:16 +0530

    changes done to resolved the issue

--- app/api/books/contents/route.ts
+++ app/api/books/contents/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
+
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
