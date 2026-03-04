b57b72c43644d85286c25f9971e442853936e24f
Jay Mawari <jaymawari31@gmail.com>
2026-03-04 22:50:29 +0530

    announcements removal

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,28 +1,28 @@
-import "../globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { ToastProvider } from "@/components/providers/toaster-provider"
-import { ConfettiProvider } from "@/components/providers/confetti-provider"
-import { Announcements } from "@/components/announcements" // Adjust this path if necessary
-import { Navbar } from "../(components)/navbar"
-import { Footer } from "../(components)/footer"
-import { Separator } from "@/components/ui/separator"
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ToastProvider } from "@/components/providers/toaster-provider";
+import { ConfettiProvider } from "@/components/providers/confetti-provider";
+import { Announcements } from "@/components/announcements"; // Adjust this path if necessary
+import { Navbar } from "../(components)/navbar";
+import { Footer } from "../(components)/footer";
+import { Separator } from "@/components/ui/separator";
 
 export const metadata: Metadata = {
   title: "Homeosetu",
 
   description:
     "Empowering you with knowledge on effective and safe homeopathy usage.",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <>
-      <Announcements />
+      {/* <Announcements /> */}
       <div className="flex flex-col min-h-screen mt-6">
         <div className="flex-grow pt-2">
           {" "}
@@ -38,5 +38,5 @@ export default function RootLayout({
         </div>
       </div>
     </>
-  )
+  );
 }
