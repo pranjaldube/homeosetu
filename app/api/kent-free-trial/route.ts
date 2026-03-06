4733e67ecf4a4d60dcbc2d038173313a2bb140b1
Jay Mawari <jaymawari31@gmail.com>
2026-03-06 18:15:59 +0530

    error resolved

--- app/api/kent-free-trial/route.ts
+++ app/api/kent-free-trial/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request) {
   try {
     const { userId } = await auth();
